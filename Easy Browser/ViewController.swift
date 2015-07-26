b5fb4bf047e860ba5b58c636ed6f20f44fb0da83
macbellingrath <macbellingrath@gmail.com>
2015-07-26 12:19:24 -0500

    Added Browsing Functionality and screenshots

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -7,19 +7,94 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    var websites = ["https://www.apple.com","https://www.twitter.com","https://www.google.com"]
+    var progressView:UIProgressView!
+    var inputTextField:UITextField?
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+        
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        navigationController?.navigationBar.backgroundColor = UIColor.blackColor()
+        UIToolbar.appearance().barTintColor = UIColor.whiteColor()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: UIBarButtonItemStyle.Plain, target: self, action: "openTapped")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Add", style: .Plain, target: self, action: "addButtonTapped")
+        let url = NSURL(string: websites[0])!
+        webView.loadRequest(NSURLRequest(URL: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        progressView = UIProgressView(progressViewStyle: UIProgressViewStyle.Default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh, target: webView, action: "reload")
+        toolbarItems = [spacer,progressButton, refresh, spacer]
+        navigationController?.toolbarHidden = false
+        
+        
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
+        
 
+    }
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .Default, handler: openPage))
+        }
+        presentViewController(ac, animated: true, completion: nil)
+        
+        
+    }
+    func addButtonTapped(){
+        let ac = UIAlertController(title: "Add", message: "Add your favorite site", preferredStyle: UIAlertControllerStyle.Alert)
+        ac.addTextFieldWithConfigurationHandler { (textfield: UITextField!) -> Void in
+            textfield.placeholder = "http://www."
+            self.inputTextField = textfield
+            
+        }
+        ac.addAction(UIAlertAction(title: "Add", style: .Default, handler: addBookmark))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    func addBookmark(UIAlertAction!) {
+       websites.append((inputTextField?.text)!)
+        print("added")
+        print(websites)
+        
+        
+    }
+    
+    func openPage(action: UIAlertAction!){
+        let url = NSURL(string: action.title!)!
+        webView.loadRequest(NSURLRequest(URL: url))
+        
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [NSObject : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
 
 }
 
